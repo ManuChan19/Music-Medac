c9b174060007f080cc108244f0b305c730b54ea6
manutosti <manutosti@gmail.com>
2024-12-18 13:28:14 +0100

    18/12/2024 falta cuenta atras

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+//Reproductor de musica
 
 function playtrack(track) {
     var audio = document.getElementById('audioPlayer')
@@ -11,14 +12,35 @@ function audioPlay() {
     audio.play();
 }
 
-function audioPause(){
+function audioPause() {
     var audio = document.getElementById('audioPlayer')
     audio.pause();
 }
 
-function audioStop(){
+function audioStop() {
     var audio = document.getElementById('audioPlayer')
     audio.src = audio.src;
-    audio.play();
-    audio.pause();
 }
+
+//Cuenta atrás ofertas
+
+let fechaFinal = new Date("Dec 18, 2024 12:55:00").getTime();
+
+let x = setInterval(function(){
+    let now = new Date().getTime();
+    let distancia = fechaFinal - now;
+
+    //calcular dias, horas, min y seg
+    let dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+    //insertamos el reloj
+    document.getElementById("cuentaatras").innerHTML = dias + "d "+ horas + "h "+ minutos + "m "+ segundos + "s ";
+    if (distancia<0){
+        fechaFinal += 604800000;
+    }
+    
+
+},1000);
